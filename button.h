cf497ab7992935e7bb7f65731f9edc89ebe30a53
Marco Giammarini <m.giammarini@warcomeb.it>
2014-06-26 00:08:53 +0200

    Added files to manage Grove Button sensor.

---
+++ button.h
@@ -0,0 +1,48 @@
+/******************************************************************************
+ * Copyright (C) 2014 A. C. Open Hardware Ideas Lab
+ * 
+ * Authors:
+ *  Marco Giammarini <m.giammarini@warcomeb.it>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+
+/**
+ * @file libohigrove/button.c
+ * @author Marco Giammarini <m.giammarini@warcomeb.it>
+ * @brief 
+ */
+
+#ifndef __BUTTON_H
+#define __BUTTON_H
+
+#include "ohigrove.h"
+
+typedef struct _OhiGroveButton_Device
+{
+    OhiGrove_Conn connector;
+    Gpio_Pins pin;
+
+} OhiGroveButton_Device;
+
+void OhiGroveButton_init (OhiGroveButton_Device* dev);
+
+Gpio_Level OhiGroveButton_get (OhiGroveButton_Device* dev);
+
+#endif /* __BUTTON_H */
