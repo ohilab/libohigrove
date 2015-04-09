cbc0b18e184a4cd83ff32b231afbfc7468ae1dc3
Marco Giammarini <m.giammarini@warcomeb.it>
2015-04-09 22:57:08 +0200

    Added vibration motor library

---
+++ vibration-motor.c
@@ -0,0 +1,61 @@
+/******************************************************************************
+ * Copyright (C) 2015 A. C. Open Hardware Ideas Lab
+ * 
+ * Authors:
+ *  Marco Giammarini <m.giammarini@warcomeb.it>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+
+/**
+ * @file libohigrove/vibration-motor.c
+ * @author Marco Giammarini <m.giammarini@warcomeb.it>
+ * @brief 
+ */
+
+#include "vibration-motor.h"
+
+void OhiGroveVibrationMotor_init (OhiGroveVibrationMotor_Device* dev)
+{
+    dev->pin = OhiGrove_getDigitalPin(dev->connector);
+
+    if (dev->pin != GPIO_PINS_NONE)
+    {
+        Gpio_config(dev->pin,GPIO_PINS_OUTPUT);
+    }
+    else
+    {
+        return; /* TODO: RETURN ERROR */
+    }
+}
+
+void OhiGroveVibrationMotor_on (OhiGroveVibrationMotor_Device* dev)
+{
+    Gpio_set(dev->pin);
+}
+
+void OhiGroveVibrationMotor_off (OhiGroveVibrationMotor_Device* dev)
+{
+    Gpio_clear(dev->pin);
+}
+
+void OhiGroveVibrationMotor_toggle (OhiGroveVibrationMotor_Device* dev)
+{
+    Gpio_toggle(dev->pin);
+}
