ad9f6bc69caa3db0e63afa95f17ca76ec69cc449
Marco Giammarini <m.giammarini@warcomeb.it>
2015-04-09 16:11:13 +0200

    Added library for light sensor http://www.seeedstudio.com/wiki/Grove_-_Light_Sensor

---
+++ light-sensor.h
@@ -0,0 +1,52 @@
+/******************************************************************************
+ * Copyright (C) 2015 A. C. Open Hardware Ideas Lab
+ * 
+ * Authors:
+ *  Marco Giammarini <m.giammarini@warcomeb.it>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+
+/**
+ * @file libohigrove/light-sensor.h
+ * @author Marco Giammarini <m.giammarini@warcomeb.it>
+ * @brief 
+ */
+
+#ifndef __LIGHT_SENSOR_H
+#define __LIGHT_SENSOR_H
+
+#include "ohigrove.h"
+
+typedef struct _OhiGroveLightSensor_Device
+{
+    OhiGrove_Conn connector;
+
+    Adc_Pins pin;
+    Adc_ChannelNumber channel;
+    Adc_DeviceHandle device;
+
+} OhiGroveLightSensor_Device;
+
+void OhiGroveLightSensor_init (OhiGroveLightSensor_Device* dev);
+
+uint16_t OhiGroveLightSensor_get (OhiGroveLightSensor_Device* dev);
+uint16_t OhiGroveLightSensor_getRaw (OhiGroveLightSensor_Device* dev);
+
+#endif /* __LIGHT_SENSOR_H */
